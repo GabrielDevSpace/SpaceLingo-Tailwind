36622bb500748dc3e1b2c2f24fd9b65e4aff843f
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-05-17 08:44:00 -0300

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -52,7 +52,6 @@ class GoogleController extends Controller
             }
         
         } catch (Exception $e) {
-            
             return redirect()->intended('login');
         }
     }
