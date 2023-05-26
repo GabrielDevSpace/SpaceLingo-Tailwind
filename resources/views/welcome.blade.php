8d24af49885791d0607cf7fdb116b21959f2d1b2
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-05-26 11:09:41 -0300

    layout welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -219,7 +219,7 @@
     </style>
 </head>
 <body class="leading-normal tracking-normal text-gray-900" style="font-family: 'Source Sans Pro', sans-serif;">
-    <div class="h-screen pb-14 bg-right bg-cover" style="background-image:url('bg.svg');">
+    <div class="max-w-6xl mx-auto py-0 sm:px-6 lg:px-8" style="background-image:url('bg.svg');">
         <!--Nav-->
         <div class="w-full container mx-auto p-6">
 
@@ -230,9 +230,6 @@
                 <div class="flex w-1/2 justify-end content-center">
                     <a href="/login">
                         <button class="bg-purple-900 hover:bg-purple-800 text-white font-bold py-2 px-3 rounded inline-flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                            </svg>
                             <span>Login</span>
                         </button>
                     </a>
