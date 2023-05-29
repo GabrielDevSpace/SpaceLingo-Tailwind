ebe21b22ae37f82d922994f9740f9b8299539ff3
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-05-29 19:56:57 -0300

    Update search-pagination.blade.php

--- resources/views/livewire/search-pagination.blade.php
+++ resources/views/livewire/search-pagination.blade.php
@@ -29,8 +29,8 @@
                             Note {!! $sortLink !!}
                         </th>
 
-                        <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
-                            Actions
+                        <th scope="col" class="border sort px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                           Actions
                         </th>
                     </tr>
                 </thead>
