2d77737cb4e01aa1c08d4f7cbb799ca2f59c04f6
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-06-01 10:55:19 -0300

    initial simple dashboard report

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -10,6 +10,37 @@ if (!function_exists('user_id')){
     }
 }
 
+
+function getSvgSmall($resultLoop) {
+    if ($resultLoop == 'vocabulary') {
+        return '<svg class="h-4 w-4 text-gray-500" width="8" height="8" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <line x1="4" y1="20" x2="7" y2="20" />
+        <line x1="14" y1="20" x2="21" y2="20" />
+        <line x1="6.9" y1="15" x2="13.8" y2="15" />
+        <line x1="10.2" y1="6.3" x2="16" y2="20" />
+        <polyline points="5 20 11 4 13 4 20 20" />
+    </svg>';
+    } elseif ($resultLoop == 'expression') {
+        return '<svg class="h-4 w-4 text-gray-500" width="8" height="8" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
+        <line x1="12" y1="12" x2="12" y2="12.01" />
+        <line x1="8" y1="12" x2="8" y2="12.01" />
+        <line x1="16" y1="12" x2="16" y2="12.01" />
+    </svg>';
+    } else {
+        return '<svg class="h-4 w-4 text-gray-500" width="8" height="8" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M3 12h7l-3 -3m0 6l3 -3" />
+        <path d="M21 12h-7l3 -3m0 6l-3 -3" />
+        <path d="M9 6v-3h6v3" />
+        <path d="M9 18v3h6v-3" />
+    </svg>';
+    }
+}
+
+
 function getSvgHelper($resultLoop) {
     if ($resultLoop == 'vocabulary') {
         return '<svg class="h-8 w-8 text-gray-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
--- resources/views/livewire/search-pagination.blade.php
+++ resources/views/livewire/search-pagination.blade.php
@@ -1,8 +1,29 @@
 <div class="container mx-auto mt-5">
     <div class="flex justify-center">
         <div class="w-full px-4">
-            <!-- INICIO -->
-            <input type="text" class="form-input w-64 mb-4" placeholder="Search Vocabulary or Translate" wire:model="searchTerm">
+            <div class="grid grid-cols-3 content-start">
+                <div><input type="text" class="form-input w-64 mb-5" placeholder="Search Vocabulary or Translate" wire:model="searchTerm"></div>
+                <div class="max-w-sm overflow-hidden">
+                    <div class="px-6 py-1">
+                        <p class="text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            This Week
+                        </p>
+                    </div>
+                    <div class="flex content-start flex-wrap">
+
+                    </div>
+                </div>
+                <div class="max-w-sm overflow-hidden">
+                    <div class="px-6 py-1">
+                        <p class="text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            This Month
+                        </p>
+                    </div>
+                    <div class="flex content-start flex-wrap">
+
+                    </div>
+                </div>
+            </div>
             <table class="table-auto min-w-full divide-y divide-gray-200 w-full">
                 <thead>
                     <tr>
@@ -58,9 +79,7 @@
                                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                     <div class="sm:flex sm:items-start">
                                         <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <svg @click="toggleModal" class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                                            </svg>
+                                            {!! $svg !!}
                                         </div>
                                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                             <h3 class="text-sm leading-6 font-medium text-gray-900" id="modal-title">
@@ -99,7 +118,7 @@
                 </tbody>
             </table>
             <div class="mt-4 pb-4">
-               {{ $newregister->links('pagination::tailwind') }}
+                {{ $newregister->links('pagination::tailwind') }}
             </div>
             <!-- FIM -->
         </div>
