be321c219c6bfbae1cac3fe242a5257cccf2a9e6
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-06-29 21:03:21 -0300

    Update create.blade.php

--- resources/views/newregister/create.blade.php
+++ resources/views/newregister/create.blade.php
@@ -87,7 +87,7 @@
                         </div>
 
                         <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <button id="submit" onclick="this.disabled = true; this.value = 'Creating…'; this.form.submit();" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                            <button id="submitbutton" onclick="this.disabled = true; this.value = 'Creating…'; this.form.submit();" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
                                 Create
                             </button>
                         </div>
@@ -100,7 +100,7 @@
 <script>
     const noteTextarea = document.getElementById('note');
     const characterCount = document.getElementById('character-count');
-    const createButton = document.getElementById('submit');
+    const createButton = document.getElementById('submitbutton');
     noteTextarea.addEventListener('input', function() {
         const textLength = noteTextarea.value.length;
         characterCount.textContent = textLength+" Characters";
