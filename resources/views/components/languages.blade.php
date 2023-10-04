f24e8e0b31e5b3cb6910a8da8ce0c650c3f4cbd6
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-10-04 11:05:37 -0300

    Created Layout to function LANGUAGES

---
+++ resources/views/components/languages.blade.php
@@ -0,0 +1,17 @@
+<div class="p-6 lg:p-6 bg-white border-b border-gray-200">
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Your Langs:
+    </h1>
+
+    <div class="flex flex-wrap mt-4">
+
+        <div class="w-1/4 px-4 mb-4"> <!-- Cada linguagem ocupará 1/4 (25%) da largura -->
+            <div class="flex items-center">
+                <img src="https://cdn-icons-png.flaticon.com/128/206/206626.png" alt="alt" class="w-8 h-8 mr-2">
+                <span class="text-base text-gray-600">English</span>
+            </div>
+        </div>
+
+    </div>
+</div>
