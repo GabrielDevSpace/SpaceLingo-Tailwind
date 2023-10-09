6d61e98499957acd091911e479d3997bdbdcce85
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-10-09 08:17:20 -0300

    Update languages.blade.php

--- resources/views/livewire/languages.blade.php
+++ resources/views/livewire/languages.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="p-6 lg:p-6 bg-white border-b border-gray-200">
         <h1 class="mt-8 text-2xl font-medium text-gray-900">
-            Your Langs:
+            Your Lang:
         </h1>
 
         <div class="flex flex-wrap -mx-4">
