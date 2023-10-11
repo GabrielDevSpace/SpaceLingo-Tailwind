e26961b379dc1e1c9c01d3db76e626940e6ee0dc
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-10-10 22:30:30 -0300

    initial courses and topics

---
+++ resources/views/components/courses.blade.php
@@ -0,0 +1,18 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200 grid grid-cols-1 md:grid-cols-2">
+
+    <div>
+        1
+    </div>
+    <div>
+        2
+    </div>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+    <div>
+        3
+    </div>
+    <div>
+        4
+    </div>
+</div>
