b40919ed0d3f9ecece4744b22afd41eab830bf2f
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2023-10-14 12:05:21 -0300

    Design Langs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <style type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <style type="text/css">
        .sorticon{
              visibility: hidden;
              color: darkgray;
--- resources/views/livewire/languages.blade.php
+++ resources/views/livewire/languages.blade.php
@@ -6,7 +6,72 @@
 
         <div class="flex flex-wrap -mx-4">
             @foreach($languages as $language)
-            <div class="relative w-1/5 px-4 mb-4">
+            <div class="relative w-1/4 mb-4 border-2 border-violet-300 border-dashed rounded-md m-1">
+                <div class="bg-violet-100 p-2 text-center">
+                    <button wire:click="confirmDelete('{{ $language->id }}')" class="absolute top-1 left-1 px-2 py-0 bg-violet-500 text-white rounded-full hover:bg-violet-900 focus:ring-red-300 text-sm ml-0">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                    <a href="{{ route('newregister', ['id' => $language->id]) }}" class="block">
+                        <div class="flex justify-center items-center">
+                            @php
+                            $imageName = '';
+                            if ($language->name === 'English') {
+                            $imageName = 'usa.png';
+                            } elseif ($language->name === 'Spanish') {
+                            $imageName = 'spain.png';
+                            } elseif ($language->name === 'French') {
+                            $imageName = 'france.png';
+                            } elseif ($language->name === 'German') {
+                            $imageName = 'germany.png';
+                            } elseif ($language->name === 'Japanese') {
+                            $imageName = 'japan.png';
+                            } elseif ($language->name === 'Italian') {
+                            $imageName = 'italy.png';
+                            } elseif ($language->name === 'Korean') {
+                            $imageName = 'korea.png';
+                            } elseif ($language->name === 'Portuguese-Brazil') {
+                            $imageName = 'brazil.png';
+                            } elseif ($language->name === 'Portuguese-Portugal') {
+                            $imageName = 'portugal.png';
+                            } elseif ($language->name === 'Chinese') {
+                            $imageName = 'china.png';
+                            } elseif ($language->name === 'Hindi') {
+                            $imageName = 'india.png';
+                            } elseif ($language->name === 'Russian') {
+                            $imageName = 'russia.png';
+                            }
+                            @endphp
+                            <div class="text-center">
+                                <img src="{{ asset('images/countries/'.$imageName) }}" alt="{{ $language->name }}" class="w-12 h-12 align-center">
+                            </div>
+                        </div>
+                        <div>
+                            <span class="text-base text-gray-600">{{ $language->name }}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="bg-violet-200 p-2 ">
+                    <div class="grid grid-cols-2 gap-8">
+                        <div>
+                            <button wire:click="#" class="px-4 py-2 bg-violet-500 text-white rounded-full hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 text-sm" type="button">
+                                Vocabulary
+                            </button>
+                        </div>
+                        <div>
+                            <button wire:click="#" class="px-4 py-2 bg-violet-500 text-white rounded-full hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 text-sm" type="button">
+                                Notes
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <!--
+        <div class="flex flex-wrap -mx-4">
+            @foreach($languages as $language)
+            <div class="relative w-1/4 px-4 mb-4">
                 <button wire:click="confirmDelete('{{ $language->id }}')" class="absolute top-0 left-0 px-1 py-0 bg-gray-700 text-white rounded-full hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium text-xs ml-0">
                     x
                 </button>
@@ -48,7 +113,7 @@
                 </a>
             </div>
             @endforeach
-        </div>
+        </div> -->
     </div>
     @if($confirmingDelete)
     <div class="fixed inset-0 flex items-center justify-center z-50">
@@ -75,34 +140,34 @@
     </div>
     <!-- Modal -->
     @if($showAddLanguageModal)
-<div class="fixed inset-0 flex items-center justify-center z-50">
-    <!-- Elemento de fundo semi-transparente -->
-    <div class="fixed inset-0 bg-gray-900 opacity-50"></div>
-    <!-- Modal -->
-    <div class="modal-container fixed inset-0 flex items-center justify-center z-50">
-        <div class="bg-white rounded-lg shadow-lg p-6 max-w-md mx-auto" style="max-height: 33vh;">
-            <h2 class="text-xl font-semibold text-gray-900 mb-4">Add Language</h2>
-            <select wire:model.defer="newLanguage" id="languageSelect" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 focus:border-blue-500 placeholder-gray-400">
-                <option value="" disabled selected>Select a Language</option>
-                <option value="English">English</option>
-                <option value="Spanish">Spanish</option>
-                <option value="French">French</option>
-                <option value="German">German</option>
-                <option value="Japanese">Japanese</option>
-                <option value="Italian">Italian</option>
-                <option value="Korean">Korean</option>
-                <option value="Portuguese-Brazil">Portuguese (Brazil)</option>
-                <option value="Portuguese-Portugal">Portuguese (Portugal)</option>
-                <option value="Chinese">Chinese</option>
-                <option value="Hindi">Hindi</option>
-                <option value="Russian">Russian</option>
-            </select>
-            <div class="mt-4 flex justify-end">
-                <button wire:click="closeAddLanguageModal" class="px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Cancel</button>
-                <button wire:click="addLanguage" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm ml-2">Add</button>
+    <div class="fixed inset-0 flex items-center justify-center z-50">
+        <!-- Elemento de fundo semi-transparente -->
+        <div class="fixed inset-0 bg-gray-900 opacity-50"></div>
+        <!-- Modal -->
+        <div class="modal-container fixed inset-0 flex items-center justify-center z-50">
+            <div class="bg-white rounded-lg shadow-lg p-6 max-w-md mx-auto" style="max-height: 33vh;">
+                <h2 class="text-xl font-semibold text-gray-900 mb-4">Add Language</h2>
+                <select wire:model.defer="newLanguage" id="languageSelect" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200 focus:border-blue-500 placeholder-gray-400">
+                    <option value="" disabled selected>Select a Language</option>
+                    <option value="English">English</option>
+                    <option value="Spanish">Spanish</option>
+                    <option value="French">French</option>
+                    <option value="German">German</option>
+                    <option value="Japanese">Japanese</option>
+                    <option value="Italian">Italian</option>
+                    <option value="Korean">Korean</option>
+                    <option value="Portuguese-Brazil">Portuguese (Brazil)</option>
+                    <option value="Portuguese-Portugal">Portuguese (Portugal)</option>
+                    <option value="Chinese">Chinese</option>
+                    <option value="Hindi">Hindi</option>
+                    <option value="Russian">Russian</option>
+                </select>
+                <div class="mt-4 flex justify-end">
+                    <button wire:click="closeAddLanguageModal" class="px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Cancel</button>
+                    <button wire:click="addLanguage" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm ml-2">Add</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-@endif
+    @endif
 </div>
